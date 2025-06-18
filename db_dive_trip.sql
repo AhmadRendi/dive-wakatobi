cca42695112d3374b47dea27303db2f3fe4016a7
AhmadRendi <ahmadrendiajah@gmail.com>
2025-06-18 14:07:21 +0800

    new database

--- db_dive_trip.sql
+++ db_dive_trip.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 05, 2025 at 11:21 AM
+-- Generation Time: Jun 18, 2025 at 08:04 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -78,18 +78,21 @@ CREATE TABLE `tbl_paket` (
   `harga` int(11) NOT NULL,
   `picture` varchar(200) DEFAULT NULL,
   `paket` varchar(50) DEFAULT NULL,
-  `waktu` time DEFAULT NULL,
-  `lokasi` varchar(100) DEFAULT NULL
+  `lokasi` varchar(100) DEFAULT NULL,
+  `disable` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_paket`
 --
 
-INSERT INTO `tbl_paket` (`id`, `namaPaket`, `deskripsi`, `harga`, `picture`, `paket`, `waktu`, `lokasi`) VALUES
-(1, 'Paket A', ' Paket A adalah', 4000000, '67ffa877384789.15529437.jpeg', 'PENYELAM', '07:00:00', 'Wangi-Wangi'),
-(4, 'Paket A', ' paket A', 5000000, '67ffce8c085a51.97727307.jpeg', 'KURSUS', '07:00:00', 'Binongko'),
-(6, 'Paket B', ' Paket B adalah', 5000000, '680c7a02526d80.40381775.jpeg', 'PENYELAM', '07:00:00', 'Binongko');
+INSERT INTO `tbl_paket` (`id`, `namaPaket`, `deskripsi`, `harga`, `picture`, `paket`, `lokasi`, `disable`) VALUES
+(1, 'Paket A', ' Paket A adalah', 4000000, '67ffa877384789.15529437.jpeg', 'PENYELAM', 'Wangi-Wangi', 1),
+(4, 'Paket A', ' paket A', 5000000, '67ffce8c085a51.97727307.jpeg', 'KURSUS', 'Wangi-Wangi', 0),
+(6, 'Paket B', ' Paket B adalah', 5000000, '680c7a02526d80.40381775.jpeg', 'PENYELAM', 'Binongko', 0),
+(7, 'Paket A', ' vsa', 412, '68517f685fa4c9.16636390.jpeg', 'KURSUS', 'Wangi-Wangi', 1),
+(8, 'Paket A', ' sda', 10000000, '68517fd10b9416.47774274.jpeg', 'KURSUS', 'Kaledupa', 1),
+(9, 'Paket A', ' haha', 4555, '685186a6472808.74579357.jpeg', 'PENYELAM', 'Tomia', 0);
 
 --
 -- Triggers `tbl_paket`
@@ -101,23 +104,9 @@ CREATE TRIGGER `before_insert_paket` BEFORE INSERT ON `tbl_paket` FOR EACH ROW B
     -- Cek apakah kombinasi lokasi dan waktu sudah ada
     SELECT COUNT(*) INTO count_existing
     FROM tbl_paket
-    WHERE lokasi = NEW.lokasi AND waktu = NEW.waktu AND paket = NEW.paket;
+    WHERE lokasi = NEW.lokasi AND paket = NEW.paket;
 
     -- Jika ada, maka batalkan penyisipan
-    IF count_existing > 0 THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Paket dengan lokasi dan waktu yang sama sudah ada';
-    END IF;
-END
-$$
-DELIMITER ;
-DELIMITER $$
-CREATE TRIGGER `before_update_paket` BEFORE UPDATE ON `tbl_paket` FOR EACH ROW BEGIN
-    DECLARE count_existing INT;
-
-    SELECT COUNT(*) INTO count_existing
-    FROM tbl_paket
-    WHERE lokasi = NEW.lokasi AND waktu = NEW.waktu AND paket = NEW.paket;
-
     IF count_existing > 0 THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Paket dengan lokasi dan waktu yang sama sudah ada';
     END IF;
@@ -175,21 +164,22 @@ CREATE TABLE `tbl_pemesanan` (
   `status` varchar(50) DEFAULT 'Menunggu Pembayaran',
   `jmlPeserta` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
-  `namaLengkap` varchar(100) DEFAULT NULL
+  `namaLengkap` varchar(100) DEFAULT NULL,
+  `disable` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_pemesanan`
 --
 
-INSERT INTO `tbl_pemesanan` (`id`, `id_user`, `id_paket`, `id_guide`, `id_keahlian`, `tanggalPemesanan`, `status`, `jmlPeserta`, `harga`, `namaLengkap`) VALUES
-(1, 1, 1, 2, 2, '2025-04-16', 'Terverifikasi', 3, 4000000, 'User 1'),
-(2, 1, 4, 3, 1, '2025-04-17', 'DiBatalkan', 4, 5000000, 'User'),
-(3, 3, 4, 3, 3, '2025-04-29', 'DiBatalkan', 4, 5000000, 'User Kedua'),
-(4, 1, 4, 3, 2, '2025-04-28', 'Terverifikasi', 8, 5000000, 'User'),
-(5, 1, 6, 3, 2, '2025-05-19', 'Terverifikasi', 6, 5000000, 'User'),
-(6, 1, 4, 2, 2, '2025-06-05', 'Menunggu Pembayaran', 1, 5000000, 'User'),
-(7, 1, 6, 2, 2, '2025-06-05', 'Menunggu Pembayaran', 2, 5000000, 'User');
+INSERT INTO `tbl_pemesanan` (`id`, `id_user`, `id_paket`, `id_guide`, `id_keahlian`, `tanggalPemesanan`, `status`, `jmlPeserta`, `harga`, `namaLengkap`, `disable`) VALUES
+(1, 1, 1, 2, 2, '2025-04-16', 'Terverifikasi', 3, 4000000, 'User 1', 1),
+(2, 1, 4, 3, 1, '2025-04-17', 'DiBatalkan', 4, 5000000, 'User', 0),
+(3, 3, 4, 3, 3, '2025-04-29', 'DiBatalkan', 4, 5000000, 'User Kedua', 0),
+(4, 1, 4, 3, 2, '2025-04-28', 'Terverifikasi', 8, 5000000, 'User', 0),
+(5, 1, 6, 3, 2, '2025-05-19', 'Terverifikasi', 6, 5000000, 'User', 0),
+(6, 1, 4, 2, 2, '2025-06-05', 'Menunggu Pembayaran', 1, 5000000, 'User', 0),
+(7, 1, 6, 2, 2, '2025-06-05', 'Menunggu Pembayaran', 2, 5000000, 'User', 0);
 
 -- --------------------------------------------------------
 
@@ -237,7 +227,8 @@ CREATE TABLE `tbl_testimoni` (
 
 INSERT INTO `tbl_testimoni` (`id`, `id_user`, `komentar`, `rating`, `tanggal`) VALUES
 (3, 1, 'asvds', 3, '2025-05-25'),
-(4, 1, 'asvds', 3, '2025-05-25');
+(4, 1, 'asvds', 3, '2025-05-25'),
+(5, 1, 'sda', 4, '2025-06-17');
 
 -- --------------------------------------------------------
 
@@ -343,7 +334,7 @@ ALTER TABLE `tbl_keahlian`
 -- AUTO_INCREMENT for table `tbl_paket`
 --
 ALTER TABLE `tbl_paket`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tbl_pembayaran`
@@ -367,7 +358,7 @@ ALTER TABLE `tbl_pesan`
 -- AUTO_INCREMENT for table `tbl_testimoni`
 --
 ALTER TABLE `tbl_testimoni`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tb_user`
